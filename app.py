c13c47d6f2c050a672acbf6ae121ac3f19ece620
Vineet-Sawant <vineetswant@gmail.com>
2024-10-14 16:39:00 +0530

    Revert "app.py file created"
    This reverts commit cb77c7d3b58259d7959b4e820483674dcb4e9c86.

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "<html><body><h1>Flask Deployment successful</h1></body></html>\n"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
