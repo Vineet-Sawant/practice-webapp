e97bbd56783efd527483ec28522f92d2b4166969
pawan kumar <pawankumarrayal@gmail.com>
2024-10-15 15:44:08 +0530

    Updated app.py file

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Flask Deployment successful</h1></body></html>\n"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    env_variable_value = os.getenv('ENV_VARIABLE_VALUE', 200)
+    return f"<html><body><h1>Hello, This is New & Latest Flask Application! Environment Variable value = {env_variable_value}</h1></body></html>\n"
